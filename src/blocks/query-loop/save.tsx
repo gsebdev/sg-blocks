c8b3823b49e4c7d3314be7e9e1d1a2c72dec075e
Sebastien Gault <sgault.webdev@gmail.com>
2024-05-23 16:52:14 +0200

    ready for deployement 1

--- src/blocks/query-loop/save.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// @ts-ignore
-import { InnerBlocks } from '@wordpress/block-editor';
-
-export default function Save() {
-    return (
-        <InnerBlocks.Content />
-    );
-}
